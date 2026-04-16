45b7a977a6d6c3605bfa0c43d4e747fc455b2660
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-04-16 03:21:48 +0000

    feat: add `Caster` template declaration

---
+++ src/caster.h
@@ -0,0 +1,13 @@
+#ifndef INFINI_RT_CASTER_H_
+#define INFINI_RT_CASTER_H_
+
+#include "device.h"
+
+namespace infini::rt {
+
+template <Device::Type device_type>
+struct Caster;
+
+}  // namespace infini::rt
+
+#endif
