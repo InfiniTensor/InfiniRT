5af3a9889fc2514d36a97306b5cdae1fa9b37ecb
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-06-16 17:18:39 +0800

    test: add CTest infrastructure

---
+++ tests/test_helper.h
@@ -0,0 +1,30 @@
+#ifndef INFINI_RT_TEST_HELPER_H_
+#define INFINI_RT_TEST_HELPER_H_
+
+#include <cstddef>
+#include <iostream>
+#include <string_view>
+
+namespace infini::rt::test {
+
+class TestContext {
+ public:
+  bool Expect(bool condition, std::string_view message) {
+    if (condition) {
+      return true;
+    }
+
+    std::cerr << message << std::endl;
+    failed_ = true;
+    return false;
+  }
+
+  int ExitCode() const { return failed_ ? 1 : 0; }
+
+ private:
+  bool failed_{false};
+};
+
+}  // namespace infini::rt::test
+
+#endif
